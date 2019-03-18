1e927d79904c4ffe00cf0cede449521b93d5fa07
JMaio <10330349+JMaio@users.noreply.github.com>
2019-03-18 18:29:16 +0000

    start rtc driver code

--- cmos-rtc.cpp
+++ cmos-rtc.cpp
@@ -11,13 +11,36 @@
 using namespace infos::drivers;
 using namespace infos::drivers::timer;
 
-class CMOSRTC : public RTC {
-public:
+// necessary imports
+#include <infos/util/lock.h>
+#include <infos/util/printf.h>
+#include <arch/x86/pio.h>
+#include <infos/kernel/log.h>
+
+using namespace infos::util;
+using namespace infos::arch::x86;
+using namespace infos::kernel;
+
+class CMOSRTC : public RTC
+{
+  public:
 	static const DeviceClass CMOSRTCDeviceClass;
 
-	const DeviceClass& device_class() const override
+	const DeviceClass &device_class() const override
 	{
 		return CMOSRTCDeviceClass;
+	}
+
+	enum
+	{
+		cmos_address = 0x70,
+		cmos_data = 0x71
+	};
+
+	uint8_t get_rtc_register(uint8_t reg)
+	{
+		__outb(cmos_address, reg);
+		return __inb(cmos_data);
 	}
 
 	/**
@@ -25,9 +48,51 @@ public:
 	 * @param tp Populates the tp structure with the current data & time, as
 	 * given by the CMOS RTC device.
 	 */
-	void read_timepoint(RTCTimePoint& tp) override
+	void read_timepoint(RTCTimePoint &tp) override
 	{
-		// FILL IN THIS METHOD - WRITE HELPER METHODS IF NECESSARY
+		syslog.messagef(LogLevel::DEBUG, "reading rtc timepoint!");
+
+		// request interrupt lock
+		UniqueIRQLock l;
+
+		// set update bit mask
+		uint8_t update_mask = 1 << 7;
+		// set size of cmos array (up to register B + 1)
+		uint8_t rtc_size = 0xb + 1;
+		uint8_t cmos_mem[rtc_size];
+
+		// wait for update start
+		while (!(get_rtc_register(0xa) & update_mask))
+			;
+		// update has begun, wait until finished
+		while (get_rtc_register(0xa) & update_mask)
+			;
+
+		syslog.messagef(LogLevel::DEBUG, "rtc update complete!");
+
+		for (uint8_t i = 0; i < rtc_size; i++)
+		{
+			cmos_mem[i] = get_rtc_register(i);
+		}
+
+		uint8_t encoding = get_rtc_register(0xb) & 1 << 2;
+		// register_offsets = {0x0, 0x2, 0x4, 0x7, 0x8, 0x9};
+		
+		tp.seconds = cmos_mem[0x0];
+		tp.minutes = cmos_mem[0x2];
+		tp.hours = cmos_mem[0x4];
+
+		tp.day_of_month = cmos_mem[0x7];
+		tp.month = cmos_mem[0x8];
+		tp.year = cmos_mem[0x9];
+
+		for (uint8_t &r : cmos_mem)
+		{
+			syslog.messagef(LogLevel::DEBUG, "r = %8b", r);
+		}
+
+		// uint8_t update = __inb(0x71);
+		syslog.messagef(LogLevel::DEBUG, "done reading rtc timepoint!");
 	}
 };
 
